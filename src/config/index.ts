d7773a7da125a7f60edc5d80864248f34913a993
hchaejeong <hchaejeong@kaist.ac.kr>
2024-01-14 16:52:43 +0000

    Revert "implementing openai dalle"
    This reverts commit 3e36aa2a57da7f4d3650eac23f78c54a0a620172.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,9 @@
 import { ConfigFactory } from '@nestjs/config';
 
 import { DatebaseEnvironmentVariables, registerDatebaseConfig } from './database.config';
-import { OpenaiEnvironmentVariables, registerOpenaiConfig } from './openai.config';
 
-export type EnvironmentVariables = DatebaseEnvironmentVariables & OpenaiEnvironmentVariables ;
+export type EnvironmentVariables = DatebaseEnvironmentVariables ;
 
 export const loadConfigModule: Array<ConfigFactory> = [
   registerDatebaseConfig,
-  registerOpenaiConfig,
 ];
--- src/modules/codi/services/codi.service.ts
+++ src/modules/codi/services/codi.service.ts
@@ -45,8 +45,6 @@ export class CodiService {
             )
             if (selectedClothes.link) {
                 links.push(selectedClothes.link);
-            } else {
-                links.push(null);
             }
         }));
 
@@ -123,7 +121,6 @@ export class CodiService {
             }),
         );
 
-        codi.clothesIds.push(clothesId);
         codi.clothesImages.push(clothes.imageUrl);
         const styles = clothes.styles;
 
